7b78a38c71169eacde3fb7b6413db8c28c444f13
felipe <felipemoreira1@gmail.com>
2023-01-08 10:34:23 -0300

    Initial commit

---
+++ src/main/java/br/com/attornatus/ContatosAttornatusApplication.java
@@ -0,0 +1,13 @@
+package br.com.attornatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatosAttornatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatosAttornatusApplication.class, args);
+	}
+
+}
